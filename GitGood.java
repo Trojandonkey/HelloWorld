dc641a1a0e3d47d146735093d61c95539ebdce3c
Trojandonkey <kim1@iastate.edu>
2017-11-13 17:38:49 -0600

    First Commit

---
+++ GitGood.java
@@ -0,0 +1,10 @@
+public class GitGood{
+	/*
+	 *Pick windows or apple as a first platofrm please.
+	 *
+	 */
+	public static void main (String[] args) {
+		System.out.println("hello I am Jay.");
+	}
+}
+
