abac3a8be94054bbfcfaa4f90dc8c4db0c1f0f66
Joshua Slagle <33607647+Joshua-Slagle@users.noreply.github.com>
2017-11-14 16:35:03 -0600

    J Opinion
    Updated with my opinion

--- GitGood.java
+++ GitGood.java
@@ -7,6 +7,7 @@ public class GitGood{
 		System.out.println("hello I am Jay.");
 		System.out.println("hello this is Kyle. Apple sounds good to develop on first.");
 		System.out.println("Hello I am Calista. Plz tell me this");
+		System.out.println("Guten Morgen, Ich mag Apfel");
 	}
 }
 
